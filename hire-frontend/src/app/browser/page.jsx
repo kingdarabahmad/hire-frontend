b6cb3425e959e6d91dd7660c89710ab598a8bbf0
Darab Ahmad Khan <aksam25@gmail.com>
2023-09-23 12:34:20 +0000

    ui added

--- hire-frontend/src/app/browser/page.jsx
+++ hire-frontend/src/app/browser/page.jsx
@@ -5,18 +5,24 @@ import { Button } from '@mui/material';
 import SumbitTestModal from '../components/browserPageComponents/sumbitTestModal';
 
 const BrowserPage = () => {
-  const [openModal,setOpenModal]=useState(true);
-  const [testModal,setTestModal]=useState(false)
+  const [openModal, setOpenModal] = useState(true);
+  const [testModal, setTestModal] = useState(false)
   return (
     <div className='w-full'>
-      <button onClick={()=>setOpenModal(true)} className='absolute top-0 right-36 bg-pink-700 p-2 text-sm rounded-md m-2 text-white font-bold w-[150px] '>
-        Show Instructions
-      </button>
-      <button onClick={()=>setTestModal(true)} className='absolute top-0 right-4 bg-green-700 p-2 text-sm rounded-md m-2 text-white font-bold w-[100px] '>
-        Submit Test
-      </button>
-      <InstructionModal openModal={openModal} setOpenModal={setOpenModal}/>
-      <SumbitTestModal testModal={testModal} setTestModal={setTestModal}/>
+      <div className='flex flex-col gap-2 border-b-2 border-gray-500'>
+
+        <div className='flex justify-end'>
+
+          <button onClick={() => setOpenModal(true)} className='bg-pink-700 p-2 text-sm rounded-md m-2 text-white font-bold w-[150px] '>
+            Show Instructions
+          </button>
+          <button onClick={() => setTestModal(true)} className='bg-green-700 p-2 text-sm rounded-md m-2 text-white font-bold w-[100px]'>
+            Submit Test
+          </button>
+        </div>
+        <InstructionModal openModal={openModal} setOpenModal={setOpenModal} />
+        <SumbitTestModal testModal={testModal} setTestModal={setTestModal} />
+      </div>
       <div>
         Welcome Write you code here
       </div>
